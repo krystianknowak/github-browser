26ec48cd3627df9dddbd3475a07cae3b45789d93
Krystian <krystian.k.nowak@gmail.com>
2019-08-02 19:26:23 +0200

    polyfill bug fix for ie11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import styles from './assets/scss/app.scss';
-import {App} from "./app";
+import 'whatwg-fetch';
+import 'promise-polyfill/src/polyfill';
+import { App } from "./app";
 
 const app = new App();
 
